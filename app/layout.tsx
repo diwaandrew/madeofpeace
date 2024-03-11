291ccf2391e4b974146bdab84e934d25be949317
Husein Indra Kusuma <aida@Huseins-MacBook-Pro.local>
2024-03-11 22:56:07 +0700

    update:fix guest data

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import 'react-toastify/dist/ReactToastify.css';
-import { toast } from 'react-toastify';
 
 
 const inter = Inter({ subsets: ["latin"] });
